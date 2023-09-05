3bfc1a83329a001229c678a93dd1994419535c04
Antonio <avps2@aluno.ifal.edu.br>
2023-09-05 19:53:44 -0300

    backEndAtualizado02

--- BackEnd/src/ecommerceControlle.js
+++ BackEnd/src/ecommerceControlle.js
@@ -25,7 +25,26 @@ const ecommerceControlle = {
       console.error('Erro na consulta:', error);
       res.status(500).json({ error: 'Erro na consulta' });
     }
-  }
+  },
+
+
+  atualizarProduto: async (req, res) => {
+    const { codigo, novoPreco } = req.body;
+
+    // Verifique se o produto com o código fornecido existe
+    const produtoExistente = await dbSequelize.query(`SELECT * FROM products WHERE code = ${codigo}`, { type: dbSequelize.QueryTypes.SELECT });
+
+    if (!produtoExistente || produtoExistente.length === 0) {
+        return res.status(404).json({ error: 'Produto não encontrado.' });
+    }
+
+    // Atualize o valor do produto
+    await dbSequelize.query(`UPDATE products SET cost_price = ${novoPreco} WHERE code = ${codigo}`);
+
+    res.json({ msg: 'Valor do produto atualizado com sucesso.' });
+}
+
+
 };
 
 module.exports = ecommerceControlle;
--- BackEnd/src/ecommerceRouter.js
+++ BackEnd/src/ecommerceRouter.js
@@ -9,6 +9,7 @@ router.use(express.json());
 
 router.get('/todosproducts', ecommerceControlle.listaproducts);
 router.get('/todospacks', ecommerceControlle.listapacks);
+router.put('/atualizarpreco', ecommerceControlle.atualizarProduto);
 
 
 module.exports = router;
